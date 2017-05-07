9b26c4db88b3c01a139bfdd1ec00beccf453cebc
Jens Emil <je@gronbak.dk>
2017-05-07 16:25:49 -0600

    add js

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+           if (xmlhttp.status == 200) {
+               handleData(JSON.parse(xmlhttp.responseText));
+               //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('something else other than 200 was returned');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "https://spreadsheets.google.com/feeds/cells/1HvdRsBKvkkU1nYuKez0SoqdTXrZjSd3L83JLgcFMASI/1/public/basic?alt=json", true);     
+    xmlhttp.send();    
+}
+
+
+function handleData(data) {    
+    var rows = [];
+    //console.log(data.feed.entry);
+    var cells = data.feed.entry;
+    var numOfEntries = cells.length / 2;
+
+    var rowObj = {};
+    for (var i = 0; i < numOfEntries; i++){
+            
+        rowObj[cells[i*2].content.$t] = cells[i*2+1].content.$t;
+        //for (var j = 0; j < rowCols.length; j++){
+            //var keyVal = rowCols[j].split(':');
+            //rowObj[keyVal[0].trim()] = keyVal[1].trim();
+        //}        
+        
+    }
+    console.log(rowObj);
+    
+    document.getElementById("myDiv").innerHTML = JSON.stringify(rowObj);
+}
+
+//window.loadXMLDoc = loadXMLDoc;
+
+
+window.onload = () => {
+    loadXMLDoc();
+    var iframe = document.getElementById("theForm");
+    console.log(window.innerWidth);
+    iframe.width = window.innerWidth;
+    iframe.height = window.innerHeight; 
+}
